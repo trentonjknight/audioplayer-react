545e686ecd88285928d43a58b6a7978e43399a77
Trenton Knight <tknightj1995@yahoo.com>
2019-10-17 19:13:48 +0000

    zing

--- audioplayer/src/Player/player.js
+++ audioplayer/src/Player/player.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 
 function AudioPlayer(){
     const [songs,setSongs] = useState([])
+    const [player,setPlayer] = useState()
 
     useEffect(() => {
             fetch('https://assets.breatheco.de/apis/sound/songs')
@@ -9,95 +10,36 @@ function AudioPlayer(){
             .then( data => {setSongs(data)})
     }, [])
 
+let audio = new Audio();
+
+function clickTrack(panda){
+    audio.src = 'https://assets.breatheco.de/apis/sound/' + panda.url;
+    audio.play();
+}
+function stop(){
+    audio.pause();
+}
+function play(){
+    audio.src = 'https://assets.breatheco.de/apis/sound/' + url;
+    audio.play();
+}
+
     return (
         <>
-        <div>
-            <ul>
-                {songs.map((songs, index) => {
-                    return(
-                        <li key={index}> | Track: {songs.id} | Link: {songs.url}</li>
+            <div>
+            <h1>Click on any track name below to play the song!</h1>
+                <ol>
+                    {songs.map((item, index) => {
+                        return(
+                            <li key={index} onClick={()=>  {clickTrack(item)}}> {item.name} {item.url} </li>
+                        )}
                     )}
-                )}
-            </ul>
-        </div>
-        <audio controls></audio>
+                </ol>
+                <button onClick={()=> {play()}}>PLAY</button>
+                <button onClick={()=> {stop()}}>PAUSE</button>
+            </div>
+            <audio controls src="https://assets.breatheco.de/apis/sound/files/mario/songs/underworld.mp3"></audio>
         </>
     )
 }
 export default AudioPlayer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
