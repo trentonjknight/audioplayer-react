c6c34109fcc6018d3c21b973896ae1bb12e33439
Trenton Knight <tknightj1995@yahoo.com>
2019-10-21 13:38:04 +0000

    progress

--- audioplayer/src/Player/player.js
+++ audioplayer/src/Player/player.js
@@ -1,8 +1,16 @@
 import React, {useEffect, useState} from "react";
+import {
+  ic_play_arrow,
+  ic_pause,
+  ic_stop,
+  ic_skip_next,
+  ic_skip_previous,
+} from 'react-icons-kit/md/';
 
 function AudioPlayer(){
     const [songs,setSongs] = useState([])
-    const [skip,setSkip] = useState()
+    const [currentSong, setCurrentSong] = useState(0)
+    let audio = new Audio();
 
     useEffect(() => {
             fetch('https://assets.breatheco.de/apis/sound/songs')
@@ -10,7 +18,6 @@ function AudioPlayer(){
             .then( data => {setSongs(data)})
     }, [])
 
-let audio = new Audio();
 
 function clickTrack(track){
     audio.src = 'https://assets.breatheco.de/apis/sound/' + track.url;
@@ -25,24 +32,30 @@ function play(){
         audio.src = 'https://assets.breatheco.de/apis/sound/files/mario/songs/castle.mp3';
     audio.play();
 }
+const next = () => {
+if (currentSong === songs.length - 1) {
+      setCurrentSong(0);
+      return;
+    }
+    setCurrentSong(currentSong + 1);
+  };
 
-{/*function next(){
-    songs + 1
-} */}
 
     return (
         <>
-            <div>
-            <h1>Click on any track name below to play the song!</h1>
+            <div className="wrapper">
+            <h1>TJK MUSIC</h1>
+            <h2>Click on any track name below to play the song! Background credit Milan Noheji @nohoid</h2>
+                <button onClick={()=> {play()}}>PLAY</button>
+                <button onClick={()=> {stop()}}>PAUSE</button>
+                <button onClick={()=> {next()}}>NEXT</button>
                 <ol>
                     {songs.map((item, index) => {
                         return(
-                            <li key={index} onClick={()=>  {clickTrack(item)}}> {item.name} {item.url} </li>
+                            <li key={index} onClick={()=>  {clickTrack(item)}}>  {item.name}</li>
                         )}
                     )}
                 </ol>
-                <button onClick={()=> {play()}}>PLAY</button>
-                <button onClick={()=> {stop()}}>PAUSE</button>
 
 
             </div>
